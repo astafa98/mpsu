3398bcc152a7b798c44d9ee7486b55fa0f733eda
Ilya Astafyev <112830586+astafa98@users.noreply.github.com>
2024-06-27 14:17:43 +0300

    Create conditional_constructions_4.c

---
+++ conditional_constructions_4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+  
+int main()
+{
+    int a = 5;
+    
+    switch(a) {
+        case 4: 
+            printf("number = 4");
+            break;
+        case 5: 
+            printf("number = 5");
+            break;
+        case 6: 
+            printf("number = 6");
+            break;
+    }
+    
+}
