2c80704a5d11b27e90382316decfdabf0880bcdc
Ilya Astafyev <112830586+astafa98@users.noreply.github.com>
2024-07-04 14:04:37 +0300

    Create cycle.c

---
+++ cycle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int i = 0; i<=5; i++) {
+        printf("Hello world\n");
+    } 
+    
+    int count = 0;
+    while(count<=5) {
+        printf("Hello world\n");
+        count++;
+    }
+
+}
