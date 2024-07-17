0c6a9f0ebfb0df28bb43d145639ce4eff3b6a622
Ilya Astafyev <112830586+astafa98@users.noreply.github.com>
2024-07-17 12:31:16 +0300

    Update input_1.c

--- input_1.c
+++ input_1.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main()
+
+{
+    int firstVar;
+    double secondVar;
+    printf("Vvedite 1 peremennuu:\n");
+    scanf("%d", &firstVar);
+    printf("Vvedite 2 peremennuu:\n");
+    scanf("%lf", &secondVar);
+    printf("vi vveli: %d\n", firstVar);
+     printf("vi vveli: %lf", secondVar);
+}
